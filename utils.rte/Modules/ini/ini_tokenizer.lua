33464f58c403adc4a7d4c33834755c1c111be81e
MyNameIsTrez <welfje@gmail.com>
2022-09-07 17:41:32 +0200

    Found bug with lulpeg

--- utils.rte/Modules/ini/ini_tokenizer.lua
+++ utils.rte/Modules/ini/ini_tokenizer.lua
@@ -45,63 +45,38 @@ local V = l.V
 space = S(" \t\n")^0
 
 digit = R("09")
-number = C(
-		P("-")^-1 *
+number =
+	C(
+		("-" + digit) *
 		digit^0 *
 		(
 			P(".") *
 			digit^0
 		)^-1
-	) /
-	tonumber
+	)
 	* space
 
-lparen = "(" * space
-rparen = ")" * space
 expr_op = C( S('+-') ) * space
 term_op = C( S('*/') ) * space
 
 expr = space * P{
 	"EXPR";
-	EXPR = V("TERM") * ( expr_op * V("TERM") )^0 / eval,
-	TERM = V("FACT") * ( term_op * V("FACT") )^0 / eval,
-	FACT =
-		lparen * V("EXPR") * rparen / eval +
-		number / eval
+	EXPR = V("TERM") * ( ( expr_op * V("TERM") )^0 ),
+	TERM = V("FACT") * ( ( term_op * V("FACT") )^0 ),
+	FACT = number
 }
-
-function eval(...)
-	local args = {...}
-	local accum = args[1]
-
-	for i = 2, #args, 2 do
-		local operator = args[i]
-		local num2 = args[i + 1]
-
-		if operator == "+" then
-			accum = accum + num2
-		elseif operator == "-" then
-			accum = accum - num2
-		elseif operator == "*" then
-			accum = accum * num2
-		elseif operator == "/" then
-			accum = accum / num2
-		end
-	end
-
-	return accum
-end
 
 -- Use this to test this function:
 -- f, err = loadfile("utils.rte/Modules/ini/ini_tokenizer.lua") f().foo()
 function M.foo()
-	-- print(expr:match("0 + 2 * 3 * 4"))
-
-	assert(expr:match(" 1 + 2 ") == 3)
-    assert(expr:match("1+2+3+4+5") == 15)
-    assert(expr:match("2*3*4 + 5*6*7") == 234)
-    assert(expr:match(" 1 * 2 + 3") == 5)
-    assert(expr:match("( 2 +2) *6") == 24)
+	local a, b, c, d, e, f, g = expr:match("2 * 3 * 4 + 5 * 6 * 7")
+	print(a)
+	print(b)
+	print(c)
+	print(d)
+	print(e)
+	print(f)
+	print(g)
 end
 
 
