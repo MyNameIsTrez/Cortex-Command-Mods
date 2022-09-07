a13f21ff7b7cc3c9cdc04daff9499b7076b84c04
MyNameIsTrez <welfje@gmail.com>
2022-09-07 17:53:17 +0200

    Output calculator AST

--- utils.rte/Modules/ini/ini_tokenizer.lua
+++ utils.rte/Modules/ini/ini_tokenizer.lua
@@ -3,6 +3,7 @@
 
 -- local iniTokenizerTests = require("Modules.ini_tokenizer_tests")
 local l = require("Modules.ini.lulpeg")
+local utils = require("Modules.Utils")
 
 
 -- MODULE START ----------------------------------------------------------------
@@ -37,6 +38,8 @@ local C = l.C
 local P = l.P
 local R = l.R
 local V = l.V
+local Ct = l.Ct
+local Cc = l.Cc
 
 
 -- PUBLIC FUNCTIONS ------------------------------------------------------------
@@ -54,6 +57,7 @@ number =
 			digit^0
 		)^-1
 	)
+	/ tonumber
 	* space
 
 expr_op = C( S('+-') ) * space
@@ -61,22 +65,17 @@ term_op = C( S('*/') ) * space
 
 expr = space * P{
 	"EXPR";
-	EXPR = V("TERM") * ( ( expr_op * V("TERM") )^0 ),
-	TERM = V("FACT") * ( ( term_op * V("FACT") )^0 ),
+	EXPR = Ct( Cc("expr") * V("TERM") * ( expr_op * V("TERM") )^0 ),
+	TERM = Ct( Cc("term") * V("FACT") * ( term_op * V("FACT") )^0 ),
 	FACT = number
 }
 
 -- Use this to test this function:
 -- f, err = loadfile("utils.rte/Modules/ini/ini_tokenizer.lua") f().foo()
 function M.foo()
-	local a, b, c, d, e, f, g = expr:match("2 * 3 * 4 + 5 * 6 * 7")
-	print(a)
-	print(b)
-	print(c)
-	print(d)
-	print(e)
-	print(f)
-	print(g)
+	local x = expr:match("2 + 3")
+	utils.RecursivelyPrint(x)
+	print(type(x[2][2]))
 end
 
 
