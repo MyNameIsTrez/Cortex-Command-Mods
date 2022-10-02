5f65985e173710139ee915af0c1be8f1b62f6056
MyNameIsTrez <welfje@gmail.com>
2022-10-02 17:11:28 +0200

    Run tests in file order

--- modmod.rte/ini/ast_tests.lua
+++ modmod.rte/ini/ast_tests.lua
@@ -45,41 +45,44 @@ local M = {};
 function M.ast_tests()
 	local cst
 
-	cst = get_cst("path")
-	ast_test("path", {
-		{ property_pointer = cst[1][1], value_pointer = cst[1][5] },
-		{ property_pointer = cst[2][1], value_pointer = cst[2][5] }
+	cst = get_cst("comment_before_tabs")
+	ast_test("comment_before_tabs", {
+		{ property_pointer = cst[1][1], value_pointer = cst[1][5], children = {
+			{ property_pointer = cst[1][7].content[1][2], value_pointer = cst[1][7].content[1][6], children = {
+				{ property_pointer = cst[1][7].content[1][8].content[1][2], value_pointer = cst[1][7].content[1][8].content[1][6], children = {
+					{ property_pointer = cst[1][7].content[1][8].content[1][8].content[1][3], value_pointer = cst[1][7].content[1][8].content[1][8].content[1][7] },
+					{ property_pointer = cst[1][7].content[1][8].content[1][8].content[2][2], value_pointer = cst[1][7].content[1][8].content[1][8].content[2][6] }
+				}}
+			}}
+		}}
 	})
-	cst = get_cst("lstripped_tab")
-	ast_test("lstripped_tab", {
-		{ property_pointer = cst[1][1], value_pointer = cst[1][5] }
-	})
-	cst = get_cst("simple")
-	ast_test("simple", {
-		{ property_pointer = cst[1][1], value_pointer = cst[1][5] }
+	cst = get_cst("comment_in_tabs")
+	ast_test("comment_in_tabs", {
+		{ property_pointer = cst[1][1], value_pointer = cst[1][5], children = {
+			{ property_pointer = cst[1][7].content[1][2], value_pointer = cst[1][7].content[1][6], children = {
+				{ property_pointer = cst[1][7].content[1][8].content[1][2], value_pointer = cst[1][7].content[1][8].content[1][6], children = {
+					{ property_pointer = cst[1][7].content[1][8].content[1][8].content[1][4], value_pointer = cst[1][7].content[1][8].content[1][8].content[1][8] },
+					{ property_pointer = cst[1][7].content[1][8].content[1][8].content[2][2], value_pointer = cst[1][7].content[1][8].content[1][8].content[2][6] }
+				}}
+			}}
+		}}
 	})
 	ast_test("comments", {})
-	cst = get_cst("nested")
-	ast_test("nested", {
-		{ property_pointer = cst[1][1], value_pointer = cst[1][5], children = {
-			{ property_pointer = cst[1][7].content[1][2], value_pointer = cst[1][7].content[1][6] }
-		}}
-	})
-	cst = get_cst("multiple")
-	ast_test("multiple", {
-		{ property_pointer = cst[1][1], value_pointer = cst[1][5], children = {
-			{ property_pointer = cst[1][7].content[1][2], value_pointer = cst[1][7].content[1][6] }
-		}},
-		{ property_pointer = cst[2][1], value_pointer = cst[2][5], children = {
-			{ property_pointer = cst[2][7].content[1][2], value_pointer = cst[2][7].content[1][6] }
-		}}
-	})
 	cst = get_cst("complex")
 	ast_test("complex", {
 		{ property_pointer = cst[1][5], value_pointer = cst[1][9], children = {
 			{ property_pointer = cst[1][12].content[1][2], value_pointer = cst[1][12].content[1][6] },
 			{ property_pointer = cst[1][12].content[2][2], value_pointer = cst[1][12].content[2][6] },
 			{ property_pointer = cst[1][12].content[3][2], value_pointer = cst[1][12].content[3][6] }
+		}}
+	})
+	cst = get_cst("datamodule")
+	ast_test("datamodule", {
+		{ property_pointer = cst[1][1], children = {
+			{ property_pointer = cst[1][3].content[1][2], value_pointer = cst[1][3].content[1][6], children = {
+				{ property_pointer = cst[1][3].content[1][8].content[1][2], value_pointer = cst[1][3].content[1][8].content[1][6] }
+			}},
+			{ property_pointer = cst[1][3].content[2][2], value_pointer = cst[1][3].content[2][6] },
 		}}
 	})
 	cst = get_cst("deindentation_1")
@@ -124,49 +127,24 @@ function M.ast_tests()
 		{ property_pointer = cst[1][1], value_pointer = cst[1][5] },
 		{ property_pointer = cst[2][1], value_pointer = cst[2][5] }
 	})
-	cst = get_cst("spaces")
-	ast_test("spaces", {
+	cst = get_cst("lstripped_tab")
+	ast_test("lstripped_tab", {
 		{ property_pointer = cst[1][1], value_pointer = cst[1][5] }
 	})
-	cst = get_cst("comment_before_tabs")
-	ast_test("comment_before_tabs", {
+	cst = get_cst("multiple")
+	ast_test("multiple", {
 		{ property_pointer = cst[1][1], value_pointer = cst[1][5], children = {
-			{ property_pointer = cst[1][7].content[1][2], value_pointer = cst[1][7].content[1][6], children = {
-				{ property_pointer = cst[1][7].content[1][8].content[1][2], value_pointer = cst[1][7].content[1][8].content[1][6], children = {
-					{ property_pointer = cst[1][7].content[1][8].content[1][8].content[1][3], value_pointer = cst[1][7].content[1][8].content[1][8].content[1][7] },
-					{ property_pointer = cst[1][7].content[1][8].content[1][8].content[2][2], value_pointer = cst[1][7].content[1][8].content[1][8].content[2][6] }
-				}}
-			}}
+			{ property_pointer = cst[1][7].content[1][2], value_pointer = cst[1][7].content[1][6] }
+		}},
+		{ property_pointer = cst[2][1], value_pointer = cst[2][5], children = {
+			{ property_pointer = cst[2][7].content[1][2], value_pointer = cst[2][7].content[1][6] }
 		}}
 	})
-	cst = get_cst("comment_in_tabs")
-	ast_test("comment_in_tabs", {
+	cst = get_cst("nested")
+	ast_test("nested", {
 		{ property_pointer = cst[1][1], value_pointer = cst[1][5], children = {
-			{ property_pointer = cst[1][7].content[1][2], value_pointer = cst[1][7].content[1][6], children = {
-				{ property_pointer = cst[1][7].content[1][8].content[1][2], value_pointer = cst[1][7].content[1][8].content[1][6], children = {
-					{ property_pointer = cst[1][7].content[1][8].content[1][8].content[1][4], value_pointer = cst[1][7].content[1][8].content[1][8].content[1][8] },
-					{ property_pointer = cst[1][7].content[1][8].content[1][8].content[2][2], value_pointer = cst[1][7].content[1][8].content[1][8].content[2][6] }
-				}}
-			}}
+			{ property_pointer = cst[1][7].content[1][2], value_pointer = cst[1][7].content[1][6] }
 		}}
-	})
-	cst = get_cst("spaces_at_start_of_line")
-	ast_test("spaces_at_start_of_line", {
-		{ property_pointer = cst[1][1], value_pointer = cst[1][5] },
-		{ property_pointer = cst[2][1], value_pointer = cst[2][5] }
-	})
-	cst = get_cst("datamodule")
-	ast_test("datamodule", {
-		{ property_pointer = cst[1][1], children = {
-			{ property_pointer = cst[1][3].content[1][2], value_pointer = cst[1][3].content[1][6], children = {
-				{ property_pointer = cst[1][3].content[1][8].content[1][2], value_pointer = cst[1][3].content[1][8].content[1][6] }
-			}},
-			{ property_pointer = cst[1][3].content[2][2], value_pointer = cst[1][3].content[2][6] },
-		}}
-	})
-	cst = get_cst("value_on_next_line")
-	ast_test("value_on_next_line", {
-		{ property_pointer = cst[1][1], value_pointer = cst[1][5] },
 	})
 	cst = get_cst("object_and_property")
 	ast_test("object_and_property", {
@@ -175,10 +153,32 @@ function M.ast_tests()
 		}},
 		{ property_pointer = cst[2][1], value_pointer = cst[2][5] }
 	})
+	cst = get_cst("path")
+	ast_test("path", {
+		{ property_pointer = cst[1][1], value_pointer = cst[1][5] },
+		{ property_pointer = cst[2][1], value_pointer = cst[2][5] }
+	})
+	cst = get_cst("simple")
+	ast_test("simple", {
+		{ property_pointer = cst[1][1], value_pointer = cst[1][5] }
+	})
+	cst = get_cst("spaces_at_start_of_line")
+	ast_test("spaces_at_start_of_line", {
+		{ property_pointer = cst[1][1], value_pointer = cst[1][5] },
+		{ property_pointer = cst[2][1], value_pointer = cst[2][5] }
+	})
+	cst = get_cst("spaces")
+	ast_test("spaces", {
+		{ property_pointer = cst[1][1], value_pointer = cst[1][5] }
+	})
 	cst = get_cst("traditional")
 	ast_test("traditional", {
 		{ property_pointer = cst[1][1] },
 		{ property_pointer = cst[2][1], value_pointer = cst[2][5] }
+	})
+	cst = get_cst("value_on_next_line")
+	ast_test("value_on_next_line", {
+		{ property_pointer = cst[1][1], value_pointer = cst[1][5] },
 	})
 end
 
--- modmod.rte/ini/file_object_tree_tests.lua
+++ modmod.rte/ini/file_object_tree_tests.lua
@@ -46,40 +46,38 @@ local M = {};
 function M.file_object_tree_tests()
 	local ast, cst
 
-	cst = get_cst("path")
-	ast = get_ast("path")
-	file_object_tree_test("path", { file_name = "path.ini", cst = cst, properties = {
-		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer },
-		{ property_pointer = ast[2].property_pointer, value_pointer = ast[2].value_pointer }
+	cst = get_cst("comment_before_tabs")
+	ast = get_ast("comment_before_tabs")
+	file_object_tree_test("comment_before_tabs", { file_name = "comment_before_tabs.ini", cst = cst, collapsed = true, children = {
+		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer, collapsed = true, children = {
+			{ property_pointer = ast[1].children[1].property_pointer, value_pointer = ast[1].children[1].value_pointer, collapsed = true, children = {
+				{ property_pointer = ast[1].children[1].children[1].property_pointer, value_pointer = ast[1].children[1].children[1].value_pointer, properties = ast[1].children[1].children[1].children }
+			}}
+		}}
 	}})
-	cst = get_cst("lstripped_tab")
-	ast = get_ast("lstripped_tab")
-	file_object_tree_test("lstripped_tab", { file_name = "lstripped_tab.ini", cst = cst, properties = {
-		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer }
-	}})
-	cst = get_cst("simple")
-	ast = get_ast("simple")
-	file_object_tree_test("simple", { file_name = "simple.ini", cst = cst, properties = {
-		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer }
+	cst = get_cst("comment_in_tabs")
+	ast = get_ast("comment_in_tabs")
+	file_object_tree_test("comment_in_tabs", { file_name = "comment_in_tabs.ini", cst = cst, collapsed = true, children = {
+		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer, collapsed = true, children = {
+			{ property_pointer = ast[1].children[1].property_pointer, value_pointer = ast[1].children[1].value_pointer, collapsed = true, children = {
+				{ property_pointer = ast[1].children[1].children[1].property_pointer, value_pointer = ast[1].children[1].children[1].value_pointer, properties = ast[1].children[1].children[1].children }
+			}}
+		}}
 	}})
 	cst = get_cst("comments")
 	ast = get_ast("comments")
 	file_object_tree_test("comments", { file_name = "comments.ini", cst = cst })
-	cst = get_cst("nested")
-	ast = get_ast("nested")
-	file_object_tree_test("nested", { file_name = "nested.ini", cst = cst, collapsed = true, children = {
-		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer, properties = ast[1].children }
-	}})
-	cst = get_cst("multiple")
-	ast = get_ast("multiple")
-	file_object_tree_test("multiple", { file_name = "multiple.ini", cst = cst, collapsed = true, children = {
-		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer, properties = ast[1].children },
-		{ property_pointer = ast[2].property_pointer, value_pointer = ast[2].value_pointer, properties = ast[2].children }
-	}})
 	cst = get_cst("complex")
 	ast = get_ast("complex")
 	file_object_tree_test("complex", { file_name = "complex.ini", cst = cst, collapsed = true, children = {
 		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer, preset_name_pointer = ast[1].children[1].value_pointer, properties = ast[1].children }
+	}})
+	cst = get_cst("datamodule")
+	ast = get_ast("datamodule")
+	file_object_tree_test("datamodule", { file_name = "datamodule.ini", cst = cst, collapsed = true, children = {
+		{ property_pointer = ast[1].property_pointer, collapsed = true, properties = { ast[1].children[2] }, children = {
+			{ property_pointer = ast[1].children[1].property_pointer, value_pointer = ast[1].children[1].value_pointer, properties = ast[1].children[1].children },
+		}}
 	}})
 	cst = get_cst("deindentation_1")
 	ast = get_ast("deindentation_1")
@@ -106,46 +104,21 @@ function M.file_object_tree_tests()
 		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer },
 		{ property_pointer = ast[2].property_pointer, value_pointer = ast[2].value_pointer }
 	}})
-	cst = get_cst("spaces")
-	ast = get_ast("spaces")
-	file_object_tree_test("spaces", { file_name = "spaces.ini", cst = cst, properties = {
+	cst = get_cst("lstripped_tab")
+	ast = get_ast("lstripped_tab")
+	file_object_tree_test("lstripped_tab", { file_name = "lstripped_tab.ini", cst = cst, properties = {
 		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer }
 	}})
-	cst = get_cst("comment_before_tabs")
-	ast = get_ast("comment_before_tabs")
-	file_object_tree_test("comment_before_tabs", { file_name = "comment_before_tabs.ini", cst = cst, collapsed = true, children = {
-		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer, collapsed = true, children = {
-			{ property_pointer = ast[1].children[1].property_pointer, value_pointer = ast[1].children[1].value_pointer, collapsed = true, children = {
-				{ property_pointer = ast[1].children[1].children[1].property_pointer, value_pointer = ast[1].children[1].children[1].value_pointer, properties = ast[1].children[1].children[1].children }
-			}}
-		}}
+	cst = get_cst("multiple")
+	ast = get_ast("multiple")
+	file_object_tree_test("multiple", { file_name = "multiple.ini", cst = cst, collapsed = true, children = {
+		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer, properties = ast[1].children },
+		{ property_pointer = ast[2].property_pointer, value_pointer = ast[2].value_pointer, properties = ast[2].children }
 	}})
-	cst = get_cst("comment_in_tabs")
-	ast = get_ast("comment_in_tabs")
-	file_object_tree_test("comment_in_tabs", { file_name = "comment_in_tabs.ini", cst = cst, collapsed = true, children = {
-		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer, collapsed = true, children = {
-			{ property_pointer = ast[1].children[1].property_pointer, value_pointer = ast[1].children[1].value_pointer, collapsed = true, children = {
-				{ property_pointer = ast[1].children[1].children[1].property_pointer, value_pointer = ast[1].children[1].children[1].value_pointer, properties = ast[1].children[1].children[1].children }
-			}}
-		}}
-	}})
-	cst = get_cst("spaces_at_start_of_line")
-	ast = get_ast("spaces_at_start_of_line")
-	file_object_tree_test("spaces_at_start_of_line", { file_name = "spaces_at_start_of_line.ini", cst = cst, properties = {
-		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer },
-		{ property_pointer = ast[2].property_pointer, value_pointer = ast[2].value_pointer }
-	}})
-	cst = get_cst("datamodule")
-	ast = get_ast("datamodule")
-	file_object_tree_test("datamodule", { file_name = "datamodule.ini", cst = cst, collapsed = true, children = {
-		{ property_pointer = ast[1].property_pointer, collapsed = true, properties = { ast[1].children[2] }, children = {
-			{ property_pointer = ast[1].children[1].property_pointer, value_pointer = ast[1].children[1].value_pointer, properties = ast[1].children[1].children },
-		}}
-	}})
-	cst = get_cst("value_on_next_line")
-	ast = get_ast("value_on_next_line")
-	file_object_tree_test("value_on_next_line", { file_name = "value_on_next_line.ini", cst = cst, properties = {
-		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer }
+	cst = get_cst("nested")
+	ast = get_ast("nested")
+	file_object_tree_test("nested", { file_name = "nested.ini", cst = cst, collapsed = true, children = {
+		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer, properties = ast[1].children }
 	}})
 	cst = get_cst("object_and_property")
 	ast = get_ast("object_and_property")
@@ -155,11 +128,38 @@ function M.file_object_tree_tests()
 			{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer, properties = ast[1].children }
 		}
 	})
+	cst = get_cst("path")
+	ast = get_ast("path")
+	file_object_tree_test("path", { file_name = "path.ini", cst = cst, properties = {
+		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer },
+		{ property_pointer = ast[2].property_pointer, value_pointer = ast[2].value_pointer }
+	}})
+	cst = get_cst("simple")
+	ast = get_ast("simple")
+	file_object_tree_test("simple", { file_name = "simple.ini", cst = cst, properties = {
+		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer }
+	}})
+	cst = get_cst("spaces_at_start_of_line")
+	ast = get_ast("spaces_at_start_of_line")
+	file_object_tree_test("spaces_at_start_of_line", { file_name = "spaces_at_start_of_line.ini", cst = cst, properties = {
+		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer },
+		{ property_pointer = ast[2].property_pointer, value_pointer = ast[2].value_pointer }
+	}})
+	cst = get_cst("spaces")
+	ast = get_ast("spaces")
+	file_object_tree_test("spaces", { file_name = "spaces.ini", cst = cst, properties = {
+		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer }
+	}})
 	cst = get_cst("traditional")
 	ast = get_ast("traditional")
 	file_object_tree_test("traditional", { file_name = "traditional.ini", cst = cst, properties = {
 		{ property_pointer = ast[1].property_pointer },
 		{ property_pointer = ast[2].property_pointer, value_pointer = ast[2].value_pointer }
+	}})
+	cst = get_cst("value_on_next_line")
+	ast = get_ast("value_on_next_line")
+	file_object_tree_test("value_on_next_line", { file_name = "value_on_next_line.ini", cst = cst, properties = {
+		{ property_pointer = ast[1].property_pointer, value_pointer = ast[1].value_pointer }
 	}})
 end
 
