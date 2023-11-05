260189d7c313e830b3846f8bb64f16f000ea8891
MyNameIsTrez <welfje@gmail.com>
2023-11-05 17:14:04 +0100

    Change .ini extension to .ini_test to make the mod converter not crash on this mod

--- modmod.rte/ini_object_tree/test_files.lua
+++ modmod.rte/ini_object_tree/test_files.lua
@@ -7,7 +7,7 @@ local M = {}
 -- PUBLIC FUNCTIONS ------------------------------------------------------------
 
 function M.get_test_path_from_file_name(file_name)
-	return string.format("modmod.rte/ini_object_tree/ini_test_files/%s.ini", file_name)
+	return string.format("modmod.rte/ini_object_tree/ini_test_files/%s.ini_test", file_name)
 end
 
 -- PRIVATE FUNCTIONS -----------------------------------------------------------
