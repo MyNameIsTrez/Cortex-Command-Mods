3d72aabc39085cc788ad8655abd017170c637cc5
MyNameIsTrez <welfje@gmail.com>
2024-01-02 17:50:38 +0100

    Refactor `ui:_object_tree_button()`

--- utils.rte/Modules/ui.lua
+++ utils.rte/Modules/ui.lua
@@ -19,16 +19,6 @@ ui.object_tree_line_scroll_offset = 0
 
 -- CONFIGURABLE VARIABLES ------------------------------------------------------
 
--- TODO: Use color names, instead of raw values
--- TODO: The game just sets the line above and bellow the button to
--- TODO: yellow when it is hovered/clicked, but I can do more!
-ui.button_color_normal = 13 -- Red
-ui.button_color_hot = 122 -- Yellow
-ui.button_color_active = 151 -- Green
-
-ui.button_text_is_small = true
-ui.button_text_alignment = ui.alignments.center
-
 ui.light_green = 146
 ui.dark_green = 144
 ui.orange = 71
@@ -63,12 +53,13 @@ end
 function ui:filled_box_with_border(pos, size, filled_color, border_color)
 	self:_filled_box(pos, size, filled_color)
 
+	-- TODO: Call this once, with a thickness parameter of 2
 	self:_box(pos, size, border_color)
 	self:_box(pos + Vector(1, 1), size - Vector(2, 2), border_color)
 end
 
 function ui:object_tree_buttons(object_tree_strings, object_tree_width, height, depth)
-	local x_padding = ui.window_left_padding + depth * ui.pixels_of_indentation_per_depth
+	local text_x = ui.window_left_padding + depth * ui.pixels_of_indentation_per_depth
 
 	for _, v in ipairs(object_tree_strings) do
 		if type(v) == "table" then
@@ -79,26 +70,14 @@ function ui:object_tree_buttons(object_tree_strings, object_tree_width, height, 
 
 			-- TODO: An optional optimization is to return when height goes past the max height
 			if height[1] > self.object_tree_line_scroll_offset then
-				local width = object_tree_width - 2
-				local y_padding = ui.window_top_padding
-				local height_index = height[1] - ui.object_tree_line_scroll_offset
 				local text = v
-				local alignment = ui.alignments.left
+				local height_index = height[1] - ui.object_tree_line_scroll_offset
+				local text_vertical_stride = (height_index - 1) * self.text_vertical_stride
+				local y = ui.window_top_padding + text_vertical_stride
+				local pos = Vector(2, y)
+				local width = object_tree_width - 4
 
-				local y = y_padding + self.text_top_padding + (height_index - 1) * self.text_vertical_stride
-				local pos
-				if alignment == self.alignments.left then
-					pos = Vector(x_padding, y)
-				else
-					pos = Vector(width / 2, y)
-				end
-				self:_text(pos, text, self.text_is_small, alignment)
-
-				local selection_lines_x = 0
-				local selection_lines_y = y_padding + (height_index - 1) * self.text_vertical_stride
-				self:_selection_lines(selection_lines_x, selection_lines_y, width, self.dark_green)
-
-				-- self:_button(text, pos, size, x, width, y_padding, alignment)
+				self:_object_tree_button(text, pos, width, text_x)
 			end
 		end
 	end
@@ -120,24 +99,7 @@ function ui:_box(pos, size, color)
 	PrimitiveMan:DrawBoxPrimitive(world_pos, world_pos + size - Vector(1, 1), color)
 end
 
-function ui:_text(pos, text, text_is_small, alignment)
-	local world_pos = self.screen_offset + pos
-	PrimitiveMan:DrawTextPrimitive(world_pos, text, text_is_small, alignment)
-end
-
-function ui:_selection_lines(x, y, width, color)
-	local start_x = x + 4
-	local offset = Vector(width - 4 * 2 + 1, 0)
-	self:_line(Vector(start_x, y), offset, color)
-	self:_line(Vector(start_x, y + self.text_vertical_stride - 1), offset, color)
-end
-
-function ui:_line(pos, offset, color)
-	local world_pos = self.screen_offset + pos
-	PrimitiveMan:DrawLinePrimitive(world_pos, world_pos + offset, color)
-end
-
-function ui:_button(text, pos, size)
+function ui:_object_tree_button(text, pos, width, text_x)
 	local clicked = false
 
 	if self.active == text then
@@ -154,26 +116,35 @@ function ui:_button(text, pos, size)
 		end
 	end
 
+	local size = Vector(width, self.text_vertical_stride)
+
+	-- TODO: Use self.light_green and such, instead of these arbitrary, ugly colors!
+
+	-- TODO: The game just sets the line above and bellow the button to
+	-- TODO: yellow when it is hovered/clicked, but I should do more!
 	if self.active == text then
-		self:filled_box(pos, size, self.button_color_active)
+		local button_color_active = 151 -- Green
+		self:_filled_box(pos, size, button_color_active)
 	elseif self:_cursor_inside(pos, size) and self.active == nil then
-		self:filled_box(pos, size, self.button_color_hot)
+		local button_color_hot = 122 -- Yellow
+		self:_filled_box(pos, size, button_color_hot)
 
 		self.hot = text
 	else
-		self:filled_box(pos, size, self.button_color_normal)
+		local button_color_normal = 13 -- Red
+		self:_filled_box(pos, size, button_color_normal)
 
 		if self.hot == text then
 			self.hot = nil
 		end
 	end
 
-	-- TODO: text_pos needs to have a different value when button_text_is_small is false
-	local text_pos = pos
-	if self.button_text_alignment == self.alignments.center then
-		text_pos = text_pos + size / 2 + Vector(0, -5)
-	end
-	self:text(text_pos, text, self.button_text_is_small, self.button_text_alignment)
+	local text_pos = Vector(text_x, pos.Y + self.text_top_padding)
+	local text_is_small = true
+	local alignment = ui.alignments.left
+	self:_text(text_pos, text, text_is_small, alignment)
+
+	self:_selection_lines(pos, width, self.dark_green)
 
 	return clicked
 end
@@ -198,6 +169,32 @@ function ui:_cursor_inside(el_pos, size)
 
 	return (mouse_x > el_x) and (mouse_x < el_x + el_width) and (mouse_y > el_y) and (mouse_y < el_y + el_height)
 end
+
+function ui:_text(pos, text, text_is_small, alignment)
+	local world_pos = self.screen_offset + pos
+	PrimitiveMan:DrawTextPrimitive(world_pos, text, text_is_small, alignment)
+end
+
+function ui:_selection_lines(pos, width, color)
+	local padding = 2
+	local start_x = pos.X + padding
+	local offset = Vector(width - padding * 2 + 1 - pos.X, 0)
+
+	self:_line(Vector(start_x, pos.Y), offset, color)
+	self:_line(Vector(start_x, pos.Y + self.text_vertical_stride - 1), offset, color)
+end
+
+function ui:_line(pos, offset, color)
+	local world_pos = self.screen_offset + pos
+	PrimitiveMan:DrawLinePrimitive(world_pos, world_pos + offset, color)
+end
+
+-- TODO: Write
+-- function ui:_property_label(?)
+-- end
+-- TODO: Write
+-- function ui:_property_field(?)
+-- end
 
 -- MODULE END ------------------------------------------------------------------
 
