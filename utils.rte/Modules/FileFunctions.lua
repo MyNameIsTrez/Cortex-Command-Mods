d1749b0a7d0229b9ddc94f1d7732ca534badcc28
MyNameIsTrez <welfje@gmail.com>
2024-01-03 17:58:35 +0100

    Add spaces in comment lines

--- utils.rte/Modules/FileFunctions.lua
+++ utils.rte/Modules/FileFunctions.lua
@@ -6,8 +6,8 @@ local M = {}
 
 -- PUBLIC FUNCTIONS ------------------------------------------------------------
 
---Converts a table to its string representation. Use loadstring() to convert it back to a table later
---StackOverflow origin: https://stackoverflow.com/a/6081639/13279557
+-- Converts a table to its string representation. Use loadstring() to convert it back to a table later
+-- StackOverflow origin: https://stackoverflow.com/a/6081639/13279557
 function M.SerializeTable(value, separator, skipNewlines, name, depth)
 	separator = separator or "\t"
 	skipNewlines = skipNewlines or false
@@ -51,15 +51,15 @@ function M.FileExists(filepath)
 	return fileID ~= -1
 end
 
---Returns the contents of a file as a string
+-- Returns the contents of a file as a string
 function M.ReadFile(filepath)
 	if not M.FileExists(filepath) then
 		return false
 	end
 
 	local fileID = LuaMan:FileOpen(filepath, "r")
-	local strTab = {} --Storing the strings in a table and concatenating them at the end is faster than concatenating for every newly added line
-	local i = 1 --Manually tracking the index is faster than calling table.insert()
+	local strTab = {} -- Storing the strings in a table and concatenating them at the end is faster than concatenating for every newly added line
+	local i = 1 -- Manually tracking the index is faster than calling table.insert()
 
 	while not LuaMan:FileEOF(fileID) do
 		strTab[i] = LuaMan:FileReadLine(fileID)
@@ -71,14 +71,14 @@ function M.ReadFile(filepath)
 	return table.concat(strTab)
 end
 
---Beware, this overwrites whatever was already in the file!
+-- Beware, this overwrites whatever was already in the file!
 function M.WriteFile(filepath, str)
 	local fileID = LuaMan:FileOpen(filepath, "w")
-	LuaMan:FileWriteLine(fileID, str) --If you want to write across multiple lines, use the newline character \n in str
+	LuaMan:FileWriteLine(fileID, str) -- If you want to write across multiple lines, use the newline character \n in str
 	LuaMan:FileClose(fileID)
 end
 
---Beware, this overwrites whatever was already in the file!
+-- Beware, this overwrites whatever was already in the file!
 function M.WriteTableToFile(filepath, tab)
 	local tabStr = M.SerializeTable(tab)
 	M.WriteFile(filepath, "return " .. tabStr .. "\n")
