0246c5980ff62e3cb09092e6f075c55f90a59eda
MyNameIsTrez <welfje@gmail.com>
2024-02-06 21:16:57 +0100

    Add ini_writer.lua

--- modmod.rte/ini_writer.lua
+++ modmod.rte/ini_writer.lua
@@ -2,14 +2,64 @@ local file_functions = dofile("utils.rte/Modules/file_functions.lua")
 
 local M = {}
 
-function M.write(ast, path)
-	local fd = LuaMan:FileOpen(path, "w")
-	assert(fd ~= -1)
+function M.write(ast, output_path)
+	local output = {}
 
-	-- TODO: Write while recursively descending the AST
-	LuaMan:FileWriteLine(fd, "foo\n")
+	for index, node in ipairs(ast) do
+		write_recursively(node, output, 0)
 
-	LuaMan:FileClose(fd)
+		-- Don't add a trailing newline,
+		-- since write_recursively() already adds it
+		if node.property ~= nil and index < #ast - 1 then
+			output[#output + 1] = "\n"
+		end
+	end
+
+	local output_str = table.concat(output)
+	file_functions.write_file(output_path, output_str)
+end
+
+function write_recursively(node, output, depth)
+	-- Don't add an empty line
+	if node.property == nil and #node.comments == 0 then
+		return
+	end
+
+	for i = 1, depth do
+		append(output, "\t")
+	end
+
+	if node.property then
+		append(output, node.property)
+	end
+
+	if node.value then
+		append(output, " = ")
+		append(output, node.value)
+	end
+
+	if #node.comments > 0 then
+		if node.property ~= nil then
+			append(output, " ")
+		end
+
+		append(output, "//")
+
+		for _, comment in ipairs(node.comments) do
+			append(output, " ")
+			append(output, comment)
+		end
+	end
+
+	append(output, "\n")
+
+	for _, child in ipairs(node.children) do
+		write_recursively(child, output, depth + 1)
+	end
+end
+
+function append(output, str)
+	output[#output + 1] = str
 end
 
 return M
